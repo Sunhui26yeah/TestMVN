bde9cdae7a823fb1cf988d7f49e622f80f5fc7c9
hsun26 <hsun26@ncsu.edu>
2024-09-05 20:54:41 -0400

    changed x value

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 3;
+        int x = 4;
         int y = 2;
         y = x*2;
         System.out.println(y);

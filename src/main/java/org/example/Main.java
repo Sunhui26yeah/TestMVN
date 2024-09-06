bae0cf79adcf8bd2de2b92d9fd586b59a360eb11
HUI SUN <146675818+Sunhui26yeah@users.noreply.github.com>
2024-09-05 21:08:34 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 4;
-        int y = 2;
-        y = x*2;
-        System.out.println(y);
+        System.out.println(powExample(4,16));
 
     }
 }
